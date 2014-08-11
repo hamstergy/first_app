87908caffef5f9510877e7b3140f60f3453a8761
Yuriy Goryachev <hamstergy@yahoo.com>
2014-08-12 01:59:58 +0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yellowstore::Application.config.secret_key_base = '319f50b6a64ca4591e0fedefe9bdff4d60fb26c97387a9fd67900e585c58e9f37ceea99646f461a5a0f8532aaa517949a82a329dedc54c63b0acd1b03d894d50'
